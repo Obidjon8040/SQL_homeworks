7312224ef917e46eb3bf2a1d741e283de4bf7d49
Obidjon8040 <Olimjanovich.om@gmail.com>
2025-05-03 23:52:29 +0500

    Create vazifa7.sql

---
+++ lesson-7/homework/vazifa7.sql
@@ -0,0 +1,9 @@
+-- Easy-Level Tasks
+1. select min(price) as min_price from Products
+2. select MIn(salary) as min_salary from Employees
+3. select count(customerID) as Total_rows from Customers
+4. select count(distinct Category) as total_category from Products
+5. select sum(saleamount) as total_sales from Sales
+   where productid=7
+6. select AVG(age) as avg_age from Employees
+7. 
